8c562e5f7ca2a299b4e98764c60f58e5dcda2ffd
zhouyao-cloud <zhouyao@chuang-games.com>
2026-06-30 17:20:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,32 @@
-# config
+# -*- coding: utf-8 -*-
+
+REGIONS = {
+    "tw": "台湾",
+    "hk": "香港",
+    "mo": "澳门",
+}
+
+IOS_CHARTS = {
+    "top-free": "iOS免费榜",
+    "top-grossing": "iOS畅销榜",
+}
+
+ANDROID_CHARTS = {
+    "free": "Google免费榜",
+    "grossing": "Google畅销榜",
+}
+
+WATCH_LIST = [
+    "新熱血江湖：世界",
+    "尋夢絲路",
+    "天堂W",
+    "楓之谷M",
+    "RO仙境傳說：愛如初見",
+    "奇蹟MU：經典之戰",
+    "Last War: Survival",
+    "Whiteout Survival",
+    "菇勇者傳說",
+    "寒霜啟示錄",
+]
+
+FEISHU_WEBHOOK = ""
